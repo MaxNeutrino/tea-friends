63268bad039fea9fde0a452777403989c5f0e9fb
Maxim Stepovoy <max.stepovoy@gmail.com>
2016-05-15 12:13:19 +0300

    Added Spring test

---
+++ src/test/java/org/glasma/teafriend/web/AdminTest.java
@@ -0,0 +1,51 @@
+package org.glasma.teafriend.web;
+
+import org.glasma.teafriend.UserTestData;
+import org.glasma.teafriend.model.User;
+import org.glasma.teafriend.repository.UserRepository;
+import org.glasma.teafriend.util.exception.NotFoundException;
+import org.glasma.teafriend.web.User.AdminRestController;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.glasma.teafriend.UserTestData.ADMIN;
+import static org.glasma.teafriend.UserTestData.USER;
+
+public class AdminTest {
+    private static ConfigurableApplicationContext appCtx;
+    private static AdminRestController controller;
+
+    @BeforeClass
+    public static void beforeClass() {
+        appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml");
+        System.out.println("\n" + Arrays.toString(appCtx.getBeanDefinitionNames()) + "\n");
+        controller = appCtx.getBean(AdminRestController.class);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        UserRepository repository = appCtx.getBean(UserRepository.class);
+        repository.save(USER);
+        repository.save(ADMIN);
+    }
+
+    @Test
+    public void testDelete() throws Exception{
+        controller.delete(UserTestData.USER_ID);
+        Collection<User> users = controller.getAll();
+        Assert.assertEquals(users.size(), 1);
+        Assert.assertEquals(users.iterator().next(), ADMIN);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void testDeleteNotFound() throws Exception {
+        controller.delete(0);
+    }
+}
--- src/test/java/org/glasma/teafriend/web/UserAdminSpringMockTest.java
+++ src/test/java/org/glasma/teafriend/web/UserAdminSpringMockTest.java
@@ -1,42 +1,41 @@
-package org.glasma.teafriend;
+package org.glasma.teafriend.web;
 
+import org.glasma.teafriend.UserTestData;
 import org.glasma.teafriend.model.User;
 import org.glasma.teafriend.repository.UserRepository;
 import org.glasma.teafriend.util.exception.NotFoundException;
 import org.glasma.teafriend.web.User.AdminRestController;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.util.Arrays;
 import java.util.Collection;
 
 import static org.glasma.teafriend.UserTestData.ADMIN;
 import static org.glasma.teafriend.UserTestData.USER;
 
-public class AdminTest {
-    private static ConfigurableApplicationContext appCtx;
-    private static AdminRestController controller;
+@ContextConfiguration("classpath:spring/spring-app.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+public class UserAdminSpringMockTest {
 
-    @BeforeClass
-    public static void beforeClass() {
-        appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml");
-        System.out.println("\n" + Arrays.toString(appCtx.getBeanDefinitionNames()) + "\n");
-        controller = appCtx.getBean(AdminRestController.class);
-    }
+    @Autowired
+    private AdminRestController controller;
+
+    @Autowired
+    private UserRepository repository;
 
     @Before
     public void setUp() throws Exception {
-        UserRepository repository = appCtx.getBean(UserRepository.class);
         repository.save(USER);
         repository.save(ADMIN);
     }
 
     @Test
-    public void testDelete() throws Exception{
+    public void testDelete() throws Exception {
         controller.delete(UserTestData.USER_ID);
         Collection<User> users = controller.getAll();
         Assert.assertEquals(users.size(), 1);
@@ -45,6 +44,6 @@ public class AdminTest {
 
     @Test(expected = NotFoundException.class)
     public void testDeleteNotFound() throws Exception {
-        controller.delete(0);
+        controller.delete(10);
     }
 }
