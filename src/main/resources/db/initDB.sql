300314343abc9ee033b1b84b6258a2f40a453c6f
Maxim Stepovoy <max.stepovoy@gmail.com>
2016-06-04 18:51:03 +0300

    Added creation table comments, user_wish_tea and user_drunk_tea

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,6 +1,9 @@
 DROP TABLE IF EXISTS user_roles;
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS tea;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS tea CASCADE;
+DROP TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS user_wish_tea;
+DROP TABLE IF EXISTS user_drunk_tea;
 DROP SEQUENCE IF EXISTS global_seq;
 
 CREATE SEQUENCE global_seq START 100000;
@@ -13,6 +16,7 @@ CREATE TABLE users
   password   VARCHAR NOT NULL,
   registered TIMESTAMP DEFAULT now()
 );
+
 CREATE UNIQUE INDEX unique_email ON users (email);
 
 CREATE TABLE user_roles
@@ -29,5 +33,36 @@ CREATE TABLE tea
   name VARCHAR NOT NULL,
   category VARCHAR NOT NULL,
   country VARCHAR NOT NULL,
-  description VARCHAR NOT NULL
+  description VARCHAR NOT NULL,
+  rate INTEGER,
+  count_connoisseurs INTEGER
+);
+
+CREATE TABLE comments
+(
+  id INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  created TIMESTAMP DEFAULT now(),
+  message VARCHAR NOT NULL,
+  user_id INTEGER NOT NULL,
+  tea_id INTEGER NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+  FOREIGN KEY (tea_id) REFERENCES tea (id) ON DELETE CASCADE
+);
+
+CREATE TABLE user_wish_tea
+(
+  user_id INTEGER NOT NULL,
+  tea_id INTEGER NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+  FOREIGN KEY (tea_id) REFERENCES tea (id) ON DELETE CASCADE,
+  CONSTRAINT wish_tea_pkey PRIMARY KEY (user_id, tea_id)
+);
+
+CREATE TABLE user_drunk_tea
+(
+  user_id INTEGER NOT NULL,
+  tea_id INTEGER NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+  FOREIGN KEY (tea_id) REFERENCES tea (id) ON DELETE CASCADE,
+  CONSTRAINT drunk_tea_pkey PRIMARY KEY (user_id, tea_id)
 )
